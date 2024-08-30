933ec990ab2e3856ca828c94efe57b91257670b8
Rahul Wagh <rahul.wagh@jhooq.com>
2024-08-30 22:52:03 +0200

    added chapter-3

--- ec2-setup/3-ec2-setup-in-vpc-subnet/main.tf
+++ ec2-setup/3-ec2-setup-in-vpc-subnet/main.tf
@@ -1,9 +1,3 @@
-/*module "s3" {
-  source      = "./s3"
-  bucket_name = var.bucket_name
-  name        = var.name
-  environment = var.bucket_name
-}*/
 
 module "networking" {
   source                       = "./networking"
@@ -24,7 +18,6 @@ module "security_group" {
   ec2_sg_name_for_python_api = "SG for EC2 for enabling port 5000"
 }
 
-
 module "ec2" {
   source                     = "./ec2"
   ami_id                     = var.ec2_ami_id
@@ -38,24 +31,4 @@ module "ec2" {
   user_data_install_apache   = templatefile("./template/ec2_install_apache.sh", {})
 }
 
-/*module "flow_log_iam_role" {
-  source               = "./iam-role-policy"
-  flow_log_policy_name = "dev_proj_1_flow_log_role"
-}
-
-module "log_group" {
-  source         = "./log-group"
-  log_group_name = "dev-proj-1-log-group"
-  tags = {
-    Environment = "dev-1"
-    Application = "devops-project"
-  }
-}
-
-module "vpc_flow_log" {
-  source                    = "./flow-log"
-  vpc_flow_log_group_arn    = module.log_group.dev_proj_1_log_group_arn
-  vpc_id                    = module.networking.dev_proj_1_vpc_id
-  vpc_flow_log_iam_role_arn = module.flow_log_iam_role.flow_log_role_Arn
-}*/
 
